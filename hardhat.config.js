6a0df0526d905ac6c749f38c6db1dd8574e445ef
chmthd <chmth@protonmail.com>
2024-06-22 17:18:36 +1000

    Implement env variables, hardhat configuration and update project structure

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
