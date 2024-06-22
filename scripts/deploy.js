3b7c8b8b12772102bcfe35860fc0e4d5dd4a8f92
chmthd <chmth@protonmail.com>
2024-06-22 17:20:24 +1000

    Add deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = await Staking.deploy();
+    console.log("Staking contract deployed to:", staking.address);
+  
+    const fs = require('fs');
+    const contractsDir = __dirname + '/../src/contracts';
+  
+    if (!fs.existsSync(contractsDir)) {
+      fs.mkdirSync(contractsDir);
+    }
+  
+    fs.writeFileSync(
+      contractsDir + '/contract-address.json',
+      JSON.stringify({ Staking: staking.address }, undefined, 2)
+    );
+  
+    const StakingArtifact = artifacts.readArtifactSync("Staking");
+  
+    fs.writeFileSync(
+      contractsDir + '/Staking.json',
+      JSON.stringify(StakingArtifact, null, 2)
+    );
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
