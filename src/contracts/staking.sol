836e5353ec88ea72bc1ba79c8d75b2e8ffbbc23a
chmthd <chmth@protonmail.com>
2024-06-22 17:19:39 +1000

    Add basic functionality to staking contract

---
+++ src/contracts/staking.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract Staking {
+    mapping(address => uint256) public balances;
+
+    function stake() public payable {
+        require(msg.value > 0, "Amount should be greater than 0");
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+}
